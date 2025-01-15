912d2bf3df13d866216049733c176e355d33926e
MuhamadSayyidFadhil <147321755+MuhamadSayyidFadhil@users.noreply.github.com>
2025-01-15 16:12:23 +0700

    UAS BACKEND

--- backend-2024-uas-project-1/models/Alumni.js
+++ backend-2024-uas-project-1/models/Alumni.js
@@ -1,7 +1,6 @@
 const db = require('../config/database');
 
 class Alumni {
-    // Mendapatkan semua data alumni
     static getAll() {
         return new Promise((resolve, reject) => {
             const sql = 'SELECT * FROM alumni';
@@ -15,7 +14,6 @@ class Alumni {
         });
     }
 
-    // Menambahkan data alumni baru
     static create({ name, phone, address, graduation_year, status, company_name, position }) {
         return new Promise((resolve, reject) => {
             const sql =
@@ -41,7 +39,6 @@ class Alumni {
         });
     }
 
-    // Memperbarui data alumni berdasarkan ID
     static update(id, { name, phone, address, graduation_year, status, company_name, position }) {
         return new Promise((resolve, reject) => {
             const sql =
@@ -52,13 +49,12 @@ class Alumni {
                 if (err) {
                     reject(err);
                 } else {
-                    resolve(results.affectedRows > 0); // Mengembalikan true jika data berhasil diupdate
+                    resolve(results.affectedRows > 0);
                 }
             });
         });
     }
 
-    // Menghapus data alumni berdasarkan ID
     static delete(id) {
         return new Promise((resolve, reject) => {
             const sql = 'DELETE FROM alumni WHERE id = ?';
@@ -66,13 +62,12 @@ class Alumni {
                 if (err) {
                     reject(err);
                 } else {
-                    resolve(results.affectedRows > 0); // Mengembalikan true jika data berhasil dihapus
+                    resolve(results.affectedRows > 0);
                 }
             });
         });
     }
 
-    // Mencari alumni berdasarkan ID
     static find(id) {
         return new Promise((resolve, reject) => {
             const sql = 'SELECT * FROM alumni WHERE id = ?';
@@ -80,13 +75,12 @@ class Alumni {
                 if (err) {
                     reject(err);
                 } else {
-                    resolve(results[0] || null); // Mengembalikan null jika data tidak ditemukan
+                    resolve(results[0] || null);
                 }
             });
         });
     }
 
-    // Mencari alumni berdasarkan nama
     static search(name) {
         return new Promise((resolve, reject) => {
             const sql = 'SELECT * FROM alumni WHERE name LIKE ?';
